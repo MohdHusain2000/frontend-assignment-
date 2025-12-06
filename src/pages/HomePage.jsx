f2f9fbc019b78689dc827bba565306f11c240219
Mohamed Husain <mo.abdulla3000@gmail.com>
2025-12-06 12:54:59 -0800

    Optimize Homepage css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@apollo/client/react";
 import { GET_POSTS } from "../service/query"
+import "../style/Home.css"
+import { useNavigate } from "react-router-dom";
 
 //https://dev.to/lucis/update-apollo-cache-after-a-mutation-and-get-instant-benefits-on-your-ui-1c3b
 const HomePage = () =>{
+    const navigate = useNavigate();
     const { loading, error ,data } = useQuery(GET_POSTS, {
-        // the call of api differ from the Document becuase of removing input and avoid errors.
+    // the call of api differ from the Document becuase of removing input and avoid errors.
         variables:{ 
                 page: 1,
                 limit: 10
@@ -19,14 +22,20 @@ const HomePage = () =>{
     //return data of the api
     return(
     <>
-    <h2>Welcome to react </h2>
+    <button className="Addbtn" onClick={() =>navigate(`/Add`)}>Add</button>
+    <h2>Welcome to react application </h2>
+    <div className="container">
     {data?.posts?.data?.map(get => 
-        <div key={get.id}>
-        <h4>{get.id}</h4>
+        <div className="home" key={get.id}>
         <h2>{get.title}</h2>
         <p>{get.body}</p>
-        </div>
+    <div className="buttons">
+          <button className="btn" onClick={() =>navigate(`/${get.id}`)}>View</button>
+          <button className="btn" onClick={() =>navigate(`/Edit/${get.id}`)}>Edit</button>
+    </div>
+    </div>
      )}
+    </div>
     </>
     )
 }
